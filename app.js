484820ff738557ce7376db9ff29e3f542b90643f
umeshmaurya805 <umeshmaurya805@gmail.com>
2021-01-16 15:27:36 +0530

    push

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 mongoose
   .connect(
-    "mongodb+srv://umeshmaurya805:mauryakbc@cluster0.kjj6w.mongodb.net/test",
+    "mongodb+srv://umeshmaurya805:mauryakbc@cluster0.kjj6w.mongodb.net/",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }
-  )
+    })
   .then(() => console.log("Connected to DB!"))
   .catch((error) => console.log(error.message));
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
